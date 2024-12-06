abb19e10729360a37e02425ff6fb563a21f02f89
Rory <rorybennett123@gmail.com>
2024-12-06 10:05:15 +0000

    update transforms

--- FasterRCNNTraining/CustomFasterRCNN.py
+++ FasterRCNNTraining/CustomFasterRCNN.py
@@ -13,19 +13,24 @@ from torchvision.transforms import v2
 def get_training_transforms(image_size):
     return v2.Compose([
         v2.Resize((image_size, image_size)),
+        # Original transforms.
         v2.RandomAffine(degrees=30, shear=15, translate=(0.1, 0.1), scale=(0.6, 1.2)),
         v2.RandomHorizontalFlip(p=0.2),
         v2.RandomErasing(0.5, scale=(0.02, 0.08)),
-        v2.ToImage(),
-        v2.ToDtype(torch.float32, scale=True)
+        v2.ToDtype(torch.float32, scale=True),
+        v2.GaussianNoise(mean=0, sigma=0.2),
+        v2.GaussianBlur(kernel_size=(5, 5), sigma=(0.1, 2)),
+        v2.Grayscale(num_output_channels=3),
+        v2.ToImage()
     ])
 
 
 def get_validation_transforms(image_size):
     return v2.Compose([
         v2.Resize((image_size, image_size)),
-        v2.ToImage(),
-        v2.ToDtype(torch.float32, scale=True)
+        v2.ToDtype(torch.float32, scale=True),
+        v2.Grayscale(num_output_channels=3),
+        v2.ToImage()
     ])
 
 
